6d5e6f746281cdd9b67146b41a31b7b09ef66650
paulpeter naanouh <pnaanouh@gmail.com>
2019-03-12 09:40:16 +0200

    fixed mistake

--- script4.sh
+++ script4.sh
@@ -1,5 +1,5 @@
  #!/bin/bash
-echo "Please enter a sentence that includes the letter I"
+echo "Please enter a sentence that includes the letter i"
 read foo
-var=$(echo $foo | tr "I" "A")
+var=$(echo $foo | tr "{a-z}" "{A-Z}"|tr "I" "A")
 echo $var
